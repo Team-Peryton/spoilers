7ba867bcf0f2468b5f92aadb7975a7d107d79399
jj-foster <79290428+jj-foster@users.noreply.github.com>
2021-10-27 22:24:58 +0100

    Update Spoilers Drag Force.py

--- Spoilers Drag Force.py
+++ Spoilers Drag Force.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+import plotly.graph_objects as go
+
 V =np.arange(1,15,1)    # freestream velocity (m/s)
 y = float(input('Overall Spoiler Height:')) #m
 x= float(input('Overall Spoiler Length:'))  #m
@@ -6,6 +9,7 @@ L = float(input('Distance from leading edge to spoiler hinge:'))   #m
 Cd = float(input('Spoiler Drag Coeficient:'))    
 theta =  float(input('Deplyment Angle:'))  #Deployment Angle
 n = 6      # Based on flat plate approximation
+
 VBl = np.sqrt((n/(n+2))*(V**2))   # Velocity in Boundary Layer (m/s)
 rho =  1.225    #Density ((kg/m3)
 mu = 1.81e-5    # Dynamic Viscosity (kg/ms)
@@ -14,12 +18,12 @@ delta = (5*(L/Re))   # BL height (Delta 0.99)
 Heff = y - delta     # Effective spoiler height outside BL
 FT= (0.5 * Cd * rho * (V**2) * (Heff*x)) + ((delta*x) *(n/(n+2)) *0.5 *(V**2)*rho)   # N  Force on spoiler when perpendicular to surface
 alpha = 90 -theta   #Effective angle
-import math
+
 thetaI= math.radians(alpha)   #Degrees to radians conversion
 U= math.cos(thetaI)  # Value of cos(θI)
 F = FT*U     # Actual Force on plate
 print ('Spoiler Drag:', F , 'N') 
-import plotly.graph_objects as go
+
 fig =go.Figure(data=go.Scatter(x=V, y= F))
 fig.update_layout(title='Force Against Velocity', xaxis_title='elocity m/s', yaxis_title='Force N')
 fig.show()
